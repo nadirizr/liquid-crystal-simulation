f6f15860bd1e8bb7a9882ed52bb9c40703d98a8b
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-07-30 15:15:59 +0000

    Added various physical constants.

--- util.py
+++ util.py
@@ -13,8 +13,17 @@ import random
 ##################################################################
 
 # Boltzmann constant in [erg]/[K].
-#kB = 1.3806488*(10**(-16))
-kB = 1.0
+kB = 1.3806488*(10**(-16))
+#kB = 1.0
+
+# Bohr Magneton in [erg]/[G].
+miuB = 9.274009820*(10**(-21))
+
+# g factor for spin dipole moment.
+g = 1.0
+
+# Planck's reduced constant [erg]*[sec].
+h_bar = 1.0545726663*(10**-27)
 
 ##################################################################
 #                     Utility Functions                          #
