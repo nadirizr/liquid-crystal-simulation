542a67b14f7baeb487556c4597b005379801e4dd
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-07-30 21:10:50 +0000

    Added constants file for physical constants.

--- util.py
+++ util.py
@@ -8,26 +8,7 @@ import math
 import numpy
 import random
 
-##################################################################
-#                     Natural Constants                          #
-##################################################################
-
-# Boltzmann constant in [erg]/[K].
-kB = 1.3806488*(10**(-16))
-#kB = 1.0
-
-# Bohr Magneton in [erg]/[G].
-miuB = 9.274009820*(10**(-21))
-
-# g factor for spin dipole moment.
-g = 1.0
-
-# Planck's reduced constant [erg]*[sec].
-h_bar = 1.0545726663*(10**-27)
-
-##################################################################
-#                     Utility Functions                          #
-##################################################################
+from constants import *
 
 def P2(x):
     """
