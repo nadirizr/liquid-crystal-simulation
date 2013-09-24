037cf2ddf6f385727daf763bd295fdcbfee6180d
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-09-24 14:12:20 +0000

    Added an initial version of the LCS state manager.

---
+++ lc_state_manager.py
@@ -0,0 +1,71 @@
+import glob
+import os
+import pickle
+import re
+
+from lc import LiquidCrystalSystem
+
+class LiquidCrystalSystemStateManager:
+    """
+    This class manages a repository of states of LiquidCrystalSystems, and can
+    save a new one into the repository under an identifier, and load one from
+    it.
+    The states stored in the manager are persistent and can be loaded and saved
+    in different runs.
+    """
+    def __init__(self, parameters):
+        LCS_REPOSITORY_LOCATION = str(self.parameters["LCS_REPOSITORY_LOCATION"])
+        LCS_REPOSITORY_SUFFIX = str(self.parameters["LCS_REPOSITORY_SUFFIX"])
+
+        self.repository_path = LCS_REPOSITORY_LOCATION
+        state_paths = glob.glob(os.path.join(self.repository_path,
+                                             "*.%s" % LCS_REPOSITORY_SUFFIX))
+        self.state_repository = {}
+        for state_path in state_paths:
+            s = re.search(r"^.*\/(.*)\.%s" % LCS_REPOSITORY_SUFFIX, state_path)
+            if s and s.groups():
+                state_name = s.groups()[0]
+                self.state_repository[state_name] = state_path
+
+    def getStateNames(self):
+        """
+        Returns all of the state names currently held in the repository.
+        """
+        return self.state_repository.keys()
+
+    def loadState(self, state_name):
+        """
+        Loads the state with the given name and returns a LiquidCrystalSystem
+        object loaded with it.
+        If the given state name is not in the repository, None is returned.
+        """
+        if state_name not in self.state_repository:
+            return None
+
+        state_path = self.state_repository[state_name]
+        state_data = pickle.load(file(state_path, "r"))
+
+        parameters = state_data["parameters"]
+        temperature = state_data["temperature"]
+        spins = state_data["spins"]
+        locations = state_data["locations"]
+
+        return LiquidCrystalSystem(parameters=parameters,
+                                   initial_temperature=temperature,
+                                   initial_spins=spins,
+                                   initial_locations=locations)
+
+    def saveState(self, state_name, lcs):
+        """
+        Saves the state of the given liquid crystal system under the state name.
+        This state can be loaded later with loadState under the saved name.
+        If the given state name already exists, it will be overriden.
+        """
+        state_data = {
+            "parameters": lcs.parameters,
+            "temperature": lcs.temperature,
+            "spins": lcs.spins,
+            "locations": lcs.locations,
+        }
+
+        pickle.dump(state_data, file(filepath, "w"))
