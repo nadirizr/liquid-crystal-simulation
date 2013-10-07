1e6089fe188db17dc0b920f7190754472fe105f2
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-10-07 13:23:21 +0000

    Print out the current energy as well.

--- algorithms/monte_carlo_cooler.py
+++ algorithms/monte_carlo_cooler.py
@@ -11,5 +11,6 @@ class MonteCarloCoolingAlgorithm(MonteCarloAlgorithm):
         """
         current_energy = current_state.getPotentialEnergy()
         new_energy = new_state.getPotentialEnergy()
-        print "New energy: %s" % new_energy
+        print "Current Energy: %s" % current_energy
+        print "New Energy: %s" % new_energy
         return new_energy < current_energy
