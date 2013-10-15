b345421db7eb426c45e245c4b7fd0f11309dd3a5
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-10-15 14:47:44 +0000

    Small fix to run script.

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
  PYTHONPATH=`pwd`:$PYTHONPATH &&
  LD_LIBRARY_PATH=`pwd`/cpp/potentials:$LD_LIBRARY_PATH
  (python -u main.py $* 2>&1 | tee run.log) &&
- (cd output ; ls -1 *.xyz | sort > lqs.list))
+ (cd output && ls -1 *.xyz | sort > lqs.list))
