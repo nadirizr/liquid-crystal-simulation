c91fa13f5333eed31401f873a4293e447e0a98db
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-10-15 09:00:52 +0000

    Add a log file for the run as well.

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 (rm -rf output &&
  PYTHONPATH=`pwd`:$PYTHONPATH &&
  LD_LIBRARY_PATH=`pwd`/cpp/potentials:$LD_LIBRARY_PATH
- python main.py $* &&
+ (python -u main.py $* 2>&1 | tee run.log) &&
  (cd output ; ls -1 *.xyz | sort > lqs.list))
