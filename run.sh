c4551a6ba2af0d5e7804f2b2a57814492aa8ebbc
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-11-03 13:50:16 +0000

    Run script now correctly creates the list file for Aviz.

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
  PYTHONPATH=`pwd`:$PYTHONPATH &&
  LD_LIBRARY_PATH=`pwd`/cpp/potentials:$LD_LIBRARY_PATH
  (python -u main.py $* 2>&1 | tee run.log) &&
- (cd output && ls -1 *.xyz | sort > lqs.list))
+ (cd runs/`ls -t runs | head -1`/output && ls -1 *.xyz | sort > lqs.list))
