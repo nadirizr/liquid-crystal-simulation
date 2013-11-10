27d43e41d3f0bae6755bbd10102ab03d2a6cd2f0
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-11-10 13:14:23 +0000

    Copies the run.log file into the correct directory, and prints that directory at the end of the run.

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-(rm -rf output &&
- PYTHONPATH=`pwd`:$PYTHONPATH &&
+(PYTHONPATH=`pwd`:$PYTHONPATH &&
  LD_LIBRARY_PATH=`pwd`/cpp/potentials:$LD_LIBRARY_PATH
  (python -u main.py $* 2>&1 | tee run.log) &&
- (cd runs/`ls -t runs | head -1`/output && ls -1 *.xyz | sort > lqs.list))
+ (cd runs/`ls -t runs | head -1`/output && ls -1 *.xyz | sort > lqs.list) &&
+ (cp run.log runs/`ls -t runs | head -1`/run.log) &&
+ (echo && echo "Data saved in: runs/`ls -t runs | head -1`"))
