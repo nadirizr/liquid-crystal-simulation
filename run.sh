987db5eb74ac008afaedccab406a409e8d817bfa
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-11-21 22:43:14 +0000

    Copying of log file happens no matter what now.

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 (PYTHONPATH=`pwd`:$PYTHONPATH &&
  LD_LIBRARY_PATH=`pwd`/cpp/potentials:$LD_LIBRARY_PATH
  (python -u main.py $* 2>&1 | tee run.log) &&
- (cd runs/`ls -t runs | head -1`/output && ls -1 *.xyz | sort > lqs.list) &&
- (cp run.log runs/`ls -t runs | head -1`/run.log) &&
- (echo && echo "Data saved in: runs/`ls -t runs | head -1`"))
+ (cd runs/`ls -t runs | head -1`/output && ls -1 *.xyz | sort > lqs.list))
+cp run.log runs/`ls -t runs | head -1`/run.log
+echo && echo "Data saved in: runs/`ls -t runs | head -1`"
