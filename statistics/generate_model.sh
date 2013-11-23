2c4f9aac81133b2c5660c21c8a3da9140ded7959
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-11-23 12:30:30 +0000

    Added the AVIZ PNG model generation script.

---
+++ statistics/generate_model.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+#####################################################################
+# Collect all command line arguments.
+#####################################################################
+
+XYZ_PATTERN="$1"
+PNG_DIR="$2"
+MODEL="$3"
+
+if [[ -z "`ls $XYZ_PATTERN????????.xyz`" ]]; then
+  echo "Didn't find any XYZ files matching pattern: $XYZ_PATTERN????????.xyz"
+  echo "Aborting PNG model generation."
+  exit 1
+else
+  echo "XYZ files are: $XYZ_PATTERN????????.xyz"
+fi
+
+if [[ -d "$PNG_DIR" ]]; then
+  echo "Output directory for model PNGs: $PNG_DIR"
+else
+  echo "Invalid output directory for model PNGs: $PNG_DIR"
+  echo "Aborting PNG model generation."
+  exit 1
+fi
+
+if [[ -z "`which aviz`" ]]; then
+  echo "AVIZ not detected."
+  echo "Aborting PNG model generation."
+  exit 0
+fi
+
+#####################################################################
+# Process command line arguments and set basic parameters.
+#####################################################################
+
+XYZ_BASE_NAME=`basename $XYZ_PATTERN`
+XYZ_DIR_NAME=`dirname $XYZ_PATTERN`
+
+WIDTH=400
+HEIGHT=300
+
+OUT_DIR="$PNG_DIR"
+mkdir -p $OUT_DIR
+
+FONT="8x13bold"
+COLOR="yellow" 
+GEOMETRY1="${WIDTH}x${HEIGHT}+0+0"
+GEOMETRY2="${WIDTH}x${HEIGHT}!"
+
+#####################################################################
+# Gather all XYZ files and sort them in a list.
+#####################################################################
+
+ORDERED_LIST=/tmp/ordered_list
+
+XYZ_FILES="$XYZ_PATTERN????????.xyz"
+ls $XYZ_FILES | sort > $ORDERED_LIST
+
+#####################################################################
+# Create PNG files with AVIZ.
+#####################################################################
+
+FILE_LIST=(`cat $ORDERED_LIST`)
+NUM_FILES=${#FILE_LIST[*]}
+
+for XYZ_FILE in ${FILE_LIST[*]}
+do
+  # Set the name of the corresponding PNG file.
+  PNG_FILE="$XYZ_DIR_NAME`basename $XYZ_FILE .xyz`.png"
+
+  # Create a PNG with AVIZ.
+  aviz -snapq $XYZ_FILE -geometry $GEOMETRY1
+
+  # Set the dimensions correctly (width and height).
+  convert -geometry $GEOMETRY2 $PNG_FILE $PNG_FILE
+
+  # Add the text overlay.
+  convert -mattecolor blue -frame 6x6 $PNG_FILE $PNG_FILE
+  convert -font $FONT -fill $COLOR -draw "text 25,49 $MODEL" $PNG_FILE $PNG_FILE
+
+  # Move the PNG to the right place.
+  mv $PNG_FILE $OUT_DIR
+done
+
+# Cleanup.
+rm -f $ORDERED_LIST
