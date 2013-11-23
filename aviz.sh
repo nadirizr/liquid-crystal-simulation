eb368d9feaf642caa7d164c306e942556930657b
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-11-23 13:50:21 +0000

    Using the 3D VPM instead of the previous one.

--- aviz.sh
+++ aviz.sh
@@ -2,4 +2,4 @@
 
 LAST_RUN=`ls -t runs/ | grep -e "$1" | head -1`
 echo "Running AVIZ for run: $LAST_RUN"
-aviz -rm spin -vpm lqc.vpm -fl runs/$LAST_RUN/output/lqs.list
+aviz -vpm 3d.vpm -fl runs/$LAST_RUN/output/lqs.list
