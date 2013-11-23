491e44416aee67b6483520eabeaf153302c1e410
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-11-23 14:44:40 +0000

    Fixed the PYTHONPATH problem.

--- generate_stats.sh
+++ generate_stats.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-(PYTHONPATH=`pwd`:$PYTHONPATH &&
- LD_LIBRARY_PATH=`pwd`/cpp/potentials:$LD_LIBRARY_PATH &&
- (python statistics/runner.py $*))
+export PYTHONPATH=`pwd`:$PYTHONPATH
+export LD_LIBRARY_PATH=`pwd`/cpp/potentials:$LD_LIBRARY_PATH
+python statistics/runner.py $*
