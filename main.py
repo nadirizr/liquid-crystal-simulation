5b4cf607b7e849a05ee8c17bf45becde7b7a40e4
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-11-24 08:27:36 +0000

    Added a usage printout, support for overriding parameters from the command line, and more consistency with all parameters.

--- main.py
+++ main.py
@@ -9,37 +9,72 @@ from lc_state_manager import LiquidCrystalSystemStateManager
 from algorithms.monte_carlo_algorithm import MonteCarloAlgorithm
 from algorithms.new_state_selector import *
 
+def printUsage():
+    print "Usage: ./run.sh <model | previous run> [PARAMETER OVERRIDES] [PROFILE=True]"
+    print
+    print "Command line arguments:"
+    print "  model        - The name of a model in the models directory (without the .py suffix)"
+    print "  OR"
+    print "  previous run - The name of a previous run or part of it."
+    print "                 If there is a directory with this name in the runs directory that is"
+    print "                 not complete, it will ask if you would like to resume it."
+    print "                 If there is more than one directory that matches the argument, the "
+    print "                 latest one that isn't complete is taken."
+    print
+    print "Parameter overrides:"
+    print "  Any parameter from a model file can be overriden at the command line."
+    print "  For example:"
+    print "    ./runs.sh fixed INITIAL_TEMPERATURE=10.0"
+    print "    This will run with the fixed model, but override the initial temperature."
+    print
+    print "  PROFILE=True will specifically activate the profiler and generate a profile.out "
+    print "  file that can be used with the cProfile module."
+    print
+    print "-h or --help will display this usage."
+
 def parseCommandLineArgs(args):
+    """
+    Parses the command line arguments, and handles all value overrides, loading
+    of the correct parameters file, returning the parameters dictionary.
+    """
     parameters = {}
-    args = args[:]
+    args = args[1:]
 
-    parameters["profile"] = False
-    if "--profile" in args or "-p" in args:
-        parameters["profile"] = True
-        try:
-            args.remove("--profile")
-        except:
-            pass
-        try:
-            args.remove("-p")
-        except:
-            pass
+    # If we have '-h' or '--help', display usage and exit.
+    if "-h" in args or "--help" in args:
+        raise RuntimeError()
 
-    parameters["model"] = "default"
-    if len(args) > 1:
-        parameters["model"] = args[1]
+    # Treat the first parameter as a model or previous run.
+    model = "default"
+    if len(args) > 0:
+        model = args[0]
+    parameters = readParametersFromFile(model)
+    parameters["MODEL"] = model
+
+    # Treat all subsequent parameters as possible overrides of parameters.
+    parameters["PROFILE"] = False
+    for arg in args[1:]:
+        try:
+            exec arg in parameters
+        except:
+            raise RuntimeError("Invalid Parameter: '%s'" % arg)
 
     return parameters
 
 def readParametersFromFile(model):
+    """
+    Reads the parameter file for the given model.
+    """
     filename = "models/%s.py" % model
     print "Loading configuration: %s" % model
     parameters = {}
     exec file(filename, "r") in parameters
     return parameters
 
-def main(args):
-    parameters = readParametersFromFile(args["model"])
+def main(parameters):
+    """
+    Gathers all parameters and performs the actual run.
+    """
     DIMENSIONS = parameters["DIMENSIONS"]
     INITIAL_TEMPERATURE = float(parameters["INITIAL_TEMPERATURE"])
     USE_MC_HEATER = bool(parameters.get("USE_MC_HEATER", True))
@@ -55,11 +90,11 @@ def main(args):
     parameters["RUN_DIR"] = "runs/%04d%02d%02d_%02d%02d%02d_%s" % (
             current_time.tm_year, current_time.tm_mon, current_time.tm_mday,
             current_time.tm_hour, current_time.tm_min, current_time.tm_sec,
-            args["model"])
+            parameters["MODEL"])
     os.makedirs(parameters["RUN_DIR"])
 
     # Copy the model file to the run dir.
-    shutil.copy("models/%s.py" % (args["model"],), parameters["RUN_DIR"])
+    shutil.copy("models/%s.py" % (parameters["MODEL"],), parameters["RUN_DIR"])
 
     # Set up the state manager.
     lcs_manager = LiquidCrystalSystemStateManager(parameters)
@@ -111,8 +146,14 @@ def main(args):
     lcs_manager.saveState("final", lcs)
 
 if __name__ == "__main__":
-    args = parseCommandLineArgs(sys.argv)
-    if args["profile"]:
-        cProfile.run("main(args)", "profile.out")
+    try:
+        parameters = parseCommandLineArgs(sys.argv)
+    except RuntimeError as e:
+        print e
+        printUsage()
+        sys.exit(1)
+
+    if parameters["PROFILE"]:
+        cProfile.run("main(parameters)", "profile.out")
     else:
-        main(args)
+        main(parameters)
