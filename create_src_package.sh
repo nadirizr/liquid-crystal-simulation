a64d636d0f0c1ad7b18a688417ee2e08d3be973a
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-11-24 21:30:49 +0000

    Added a script for creating the source package.

---
+++ create_src_package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+svn export . lc_simulation_src
+rm -f lc_simulation_src/web/lc_simulation_src.tar.gz
+tar -cvzf web/lc_simulation_src.tar.gz lc_simulation_src
+svn commit web/lc_simulation_src.tar.gz -m "New version of LCS sources."
