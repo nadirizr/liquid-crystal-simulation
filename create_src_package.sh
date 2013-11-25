dcafafdc0922b2dc7cff1110de8cc9c92d45450a
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-11-25 10:27:20 +0000

    Delete the lc_simulation_src folder before creating it.

--- create_src_package.sh
+++ create_src_package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf lc_simulation_src
 svn export . lc_simulation_src
 rm -f lc_simulation_src/web/lc_simulation_src.tar.gz
 tar -cvzf web/lc_simulation_src.tar.gz lc_simulation_src
