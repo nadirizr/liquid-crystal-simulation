4b79eae68c10692e7b37beb7ee20aabd51e82d36
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-11-26 10:25:04 +0000

    create_src_package.sh does better cleanup now.

--- create_src_package.sh
+++ create_src_package.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Prepare source directory.
 rm -rf lc_simulation_src
 svn export . lc_simulation_src
 rm -f lc_simulation_src/web/lc_simulation_src.tar.gz
+
+# Create the source tar file.
 tar -cvzf web/lc_simulation_src.tar.gz lc_simulation_src
+rm -rf lc_simulation_src
+
+# Commit the new file.
 svn commit web/lc_simulation_src.tar.gz -m "New version of LCS sources."
