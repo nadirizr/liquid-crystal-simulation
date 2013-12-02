0652c62bc1f16e0153c3da06ca324db9f8351cce
nadir.izr@gmail.com <nadir.izr@gmail.com@46a8980d-f154-765a-25b4-51490709f4df>
2013-12-02 12:03:36 +0000

    Added a web package.

--- create_src_package.sh
+++ create_src_package.sh
@@ -3,11 +3,23 @@
 # Prepare source directory.
 rm -rf lc_simulation_src
 svn export . lc_simulation_src
-rm -f lc_simulation_src/web/lc_simulation_src.tar.gz
+rm -rf lc_simulation_src/web
+rm -rf lc_simulation_src/runs
 
 # Create the source tar file.
 tar -cvzf web/lc_simulation_src.tar.gz lc_simulation_src
 rm -rf lc_simulation_src
 
+# Prepare web directory.
+rm -rf lc_simulation_src
+mkdir lc_simulation_src
+svn export ./web lc_simulation_src/web
+svn export ./runs lc_simulation_src/runs
+rm -f lc_simulation_src/web/lc_simulation_src.tar.gz lc_simulation_src/web/lc_simulation_web.tar.gz
+
+# Create the web tar file.
+tar -cvzf web/lc_simulation_web.tar.gz lc_simulation_src
+rm -rf lc_simulation_src
+
 # Commit the new file.
-svn commit web/lc_simulation_src.tar.gz -m "New version of LCS sources."
+svn commit web/lc_simulation_src.tar.gz web/lc_simulation_web.tar.gz -m "New version of LCS sources and web."
